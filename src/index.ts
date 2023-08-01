ec139b5b430d7d3585aeb4b20f18911db7af4eab
Michał Sałek <contact@michalsalek.pl>
2023-08-02 01:01:41 +0200

    working simply mailing service ver 1

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
-import http from 'http'
+import { sendEmail, setApiKey }       from '@msalek/emails'
+import cors                           from 'cors'
+import * as dotenv                    from 'dotenv'
+import express, { Request, Response } from 'express'
+import helmet                         from 'helmet'
 
 
 
-// create the http server
-const server = http.createServer((req, res) => {
 
-    // Creating a task
-    if (req.method == "POST" && req.url == "/api/tasks") {
-        return addTask(req, res);
-    }
+dotenv.config()
 
-});
 
-// set up the server port and listen for connections
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
+if (!process.env.PORT || !process.env.SENDGRID_API_KEY) {
+    console.error('missing necessary env variables.')
+    process.exit(1)
+}
+
+
+const PORT: number = parseInt(process.env.PORT as string, 10)
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+
+
+setApiKey(process.env.SENDGRID_API_KEY)
+
+
+
+app.post('/send', async (req: Request, res: Response) => {
+
+    const message = {...req.body, to: 'contact@michalsalek.com'}
+
+    sendEmail(message, process.env.VERIFIED_SENDER)
+
+    res.status(200).send('Wysłano.')
+})
