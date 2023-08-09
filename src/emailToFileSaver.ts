3272af6ffc292e67b97ab85ad0ad954d3c7b6824
Michał Sałek <contact@michalsalek.pl>
2023-08-09 17:40:28 +0200

    crate dir on the server

--- src/emailToFileSaver.ts
+++ src/emailToFileSaver.ts
@@ -9,10 +9,20 @@ import { SendEmailPayload }   from './IO.types'
 
 export const saveEmailToFile = ({subject, text, replyTo}: SendEmailPayload): void => {
 
+    const dir = '../saved_emails/'
+
+    try {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
     const content = `subject: ${subject}     text: ${text}     replyTo: ${replyTo}`
 
     fs.writeFile(
-        path.resolve(__dirname, '../saved_emails/' + getDateNowInString({getISOFormat: false, withTimestamp: false}) + ' ' + subject + '.txt')
+        path.resolve(__dirname, dir + getDateNowInString({getISOFormat: false, withTimestamp: false}) + ' ' + subject + '.txt')
         ,
         content, (err) => {
             reportIssue(err)
